9a8b4e8715f5fe97daff36202396f2a1fa8ee172
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-05-11 00:52:16 +0900

    update: add handler

--- back/internal/handler/todos_handler.go
+++ back/internal/handler/todos_handler.go
@@ -7,7 +7,7 @@ import (
 	"github.com/atoyr/backyard/internal/models"
 )
 
-func Todos(w http.ResponseWriter, r *http.Request) {
+func TodosHandler(w http.ResponseWriter, r *http.Request) {
 	todos := []models.Todo{
 		{ID: 1, Title: "Todo 1"},
 		{ID: 2, Title: "Todo 2"},
