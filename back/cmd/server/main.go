433e5da176be383aa1edd873a7442351496fd43f
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-05-11 01:06:06 +0900

    update: add index page

--- back/cmd/server/main.go
+++ back/cmd/server/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/atoyr/backyard/internal/handler"
+)
+
+func main() {
+	// Ginルーターの設定
+	r := gin.Default()
+
+	// WebSocketエンドポイント
+	r.GET("/", handler.GetIndexHandler())
+
+	// サーバー起動
+	log.Println("backend listening on :8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("server error:", err)
+	}
+}
--- back/internal/handler/index_handler.go
+++ back/internal/handler/index_handler.go
@@ -1,10 +1,15 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello Vercel Functions!</h1>")
+func GetIndexHandler() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		content := "<h1>Hello Vercel Functions!</h1>"
+		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(content))
+	}
 }
