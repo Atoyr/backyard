2e0d4c06bf78685d3e4a8e1c628e4a4c101bc843
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-05-22 23:27:21 +0900

    update: add repository

---
+++ back/repositories/task_repository.go
@@ -0,0 +1,55 @@
+package repositories
+
+import (
+	"github.com/atoyr/backyard/db"
+	"github.com/atoyr/backyard/models"
+	"github.com/jackc/pgx"
+)
+
+type TaskRepository interface {
+	GetAllTasks() ([]models.Task, error)
+	CreateTask(title string) (models.Task, error)
+	UpdateTask(id int64, title string) (models.Task, error)
+	DeleteTask(id int64) (models.Task, error)
+}
+
+type dbTaskRepository struct {
+	conn *pgx.Conn
+}
+
+func NewTaskRepository(conn *pgx.Conn) TaskRepository {
+	return &dbTaskRepository{conn: conn}
+}
+
+func (d *dbTaskRepository) GetAllTasks() ([]models.Task, error) {
+	q := db.New(d.conn)
+	ctx := d.conn.Context()
+	dbTasks, err := q.ListTasks(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var tasks []models.Task
+	for _, dbTask := range dbTasks {
+		task := models.Task{
+			ID:    dbTask.ID,
+			Title: dbTask.Title,
+			// Add other fields as necessary
+		}
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
+func (d *dbTaskRepository) CreateTask(title string) (models.Task, error) {
+	panic("not implemented")
+}
+
+func (d *dbTaskRepository) UpdateTask(id int64, title string) (models.Task, error) {
+	panic("not implemented")
+}
+
+func (d *dbTaskRepository) DeleteTask(id int64) (models.Task, error) {
+	panic("not implemented")
+}
