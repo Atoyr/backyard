e4a6c0678ba0952f4e52ba25160a113ac0027e7d
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-05-27 01:06:56 +0900

    update: codegen

--- front/codegen.ts
+++ front/codegen.ts
@@ -1,12 +1,25 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
-import { supabaseAnonKey, supabaseUrl } from './src/consts';
+import { loadEnv } from 'vite';
+
+const mode = process.env.NODE_ENV || 'local-dev';
+const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+const supabaseUrl = env.VITE_SUPABASE_URL;
+const supabaseAnonKey = env.VITE_SUPABASE_ANON_KEY;
 
 const config: CodegenConfig = {
   overwrite: true, 
   // SupabaseのGraphQLエンドポイント
   // `apikey`パラメーターは手前のAPI Gatewayを通るのに必要
-  schema: `${supabaseUrl}/graphql/v1?apikey=${supabaseAnonKey}`,
-
+  schema: [{
+    [`${supabaseUrl}/graphql/v1`]: {
+      headers: {
+        'Content-Type': 'application/json',
+        'apiKey': supabaseAnonKey,
+        'Authorization': `Bearer ${supabaseAnonKey}`,
+      },
+    },
+  }], 
   // 型を生成するクエリーがどこのファイルに記載されているか。
   // 今回は`constants.ts`のみだが、今後の拡張性も考えてとりあえず全てのtsとtsxファイルを探すよう指定
   documents: ['./src/**/*.graphql'], 
@@ -18,16 +31,28 @@ const config: CodegenConfig = {
       plugins: [
         'typescript', 
         'typescript-operations', 
-        'typescript-bue-urql', 
+        'typescript-vue-urql', 
         'fragment-matcher'
       ],
       presetConfig: {
         gqlTagName: 'gql',
       }, 
       config: {
-        withCompositionFunctions: true
+        withCompositionFunctions: true, 
+        scalars: {
+          UUID: 'string',
+          Date: 'string',
+          Time: 'string',
+          Datetime: 'string',
+          JSON: 'any',
+          BigInt: 'string',
+          Opaque: 'any',
+        },
       }
     }
+  },
+  hooks: {
+    afterAllFileWrite: ['prettier --write'],
   },
   ignoreNoDocuments: true,
 };
