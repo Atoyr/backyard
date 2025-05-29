c90513391bf45a5bf1bcdb3a25d933daca9407ac
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-05-30 08:38:23 +0900

    update: shadcn/ui

--- front/src/views/todos/index.vue
+++ front/src/views/todos/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import { Button } from '@/components/ui/button';
+import ButtonX from '@/components/ButtonX.vue';
 
 const route = useRoute();
 </script>
@@ -7,6 +9,9 @@ const route = useRoute();
 <template>
   <div >
     <h1>Hello todos page</h1>
+    <Button>TEST BUTTON</Button>
+    <Button variant="destructive">TEST DENGER</Button>
+    <ButtonX>TEST BUTTON</ButtonX>
+    <ButtonX variant="destructive">TEST DENGER</ButtonX>
   </div>
 </template>
-
